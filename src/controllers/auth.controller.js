34f4b3b02811e227b3ab895fc662a23728bc52f7
Ali Talha Çoban <cobanalitalha@gmail.com>
2024-10-12 12:56:49 +0300

    Add login functionality

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,8 +1,6 @@
 const dbConnection = require("../../config/db");
 const bcrypt = require("bcryptjs");
 
-const login = async (req, res) => {};
-
 const checkIfUserExists = (email) => {
   const query = `SELECT * FROM users WHERE email = ?`;
   return new Promise((resolve, reject) => {
@@ -53,6 +51,40 @@ const register = async (req, res) => {
     return res.status(500).json({
       message: "Failed to register user",
       error: error.message,
+    });
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const ifUserExists = await checkIfUserExists(email);
+
+    if (!ifUserExists) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const query = `SELECT * FROM users WHERE email = ?`;
+
+    dbConnection.query(query, [email], async (err, result) => {
+      if (err) {
+        return res.status(500).json({
+          message: "Failed to login",
+          error: err.message,
+        });
+      }
+      const user = result[0];
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+      if (!isPasswordValid) {
+        return res.status(400).json({ message: "Invalid credentials" });
+      }
+      return res.status(200).json({ message: "User logged in successfully" });
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: "Failed to login",
+      error: err.message,
     });
   }
 };
--- src/server.js
+++ src/server.js
@@ -3,9 +3,13 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 require("../config/db");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 //Middlewares
 app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
 
 //Routes
 app.use("/api/v1/", require("./routes/auth.route"));
