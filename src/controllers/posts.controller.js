cada7c5a2ede64e2e11dab825d00d886f8f3478a
Ali Talha Çoban <cobanalitalha@gmail.com>
2024-10-13 13:42:03 +0300

    Refactor getPosts query to include posts from followed users

--- src/controllers/posts.controller.js
+++ src/controllers/posts.controller.js
@@ -2,9 +2,12 @@ const dbConnection = require("../../config/db");
 const getCurrentDate = require("../helpers/getCurrentDate");
 
 const getPosts = async (req, res) => {
-  const user = req.user;
-
-  const query = `SELECT p.*, u.id AS userId, name FROM posts AS p JOIN users AS u ON (u.id = p.user_id) ORDER BY p.created_at DESC`;
+  const query = `SELECT p.*, u.id AS userId, name 
+                 FROM posts AS p 
+                 JOIN users AS u ON (u.id = p.user_id)
+                 LEFT JOIN relationships AS r ON (p.user_id = r.followed_user_id) 
+                 WHERE r.follower_user_id = ? OR p.user_id = ?
+                 ORDER BY p.createdAt DESC`;
 
   dbConnection.query(query, (err, result) => {
     if (err) {
@@ -13,6 +16,8 @@ const getPosts = async (req, res) => {
         error: err.message,
       });
     }
+
+    result;
     return res.status(200).json({ posts: result });
   });
 };
