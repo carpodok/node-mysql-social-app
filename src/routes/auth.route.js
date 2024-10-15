14215427c70dfc1b291db2725a92a774960453d2
Ali Talha Çoban <cobanalitalha@gmail.com>
2024-10-15 11:04:33 +0300

    Add validation for authenticcation route

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,9 +1,13 @@
 const router = require("express").Router();
 
 const { login, register, logout } = require("../controllers/auth.controller");
+const {
+  loginValidator,
+  registerValidator,
+} = require("../middlewares/validators/auth.validator");
 
-router.post("/login", login);
-router.post("/register", register);
+router.post("/login", loginValidator, login);
+router.post("/register", registerValidator, register);
 router.get("/logout", logout);
 
 module.exports = router;
