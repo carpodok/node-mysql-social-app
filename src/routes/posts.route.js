6ee56020524a2ef50ff1317740dd627326ecbbb1
Ali Talha Çoban <cobanalitalha@gmail.com>
2024-10-15 11:17:14 +0300

    Refactor posts route to include validation middleware

--- src/routes/posts.route.js
+++ src/routes/posts.route.js
@@ -5,9 +5,14 @@ const {
   deletePost,
 } = require("../controllers/posts.controller");
 const verifyAuth = require("../middlewares/verifyAuth");
+const {
+  getPostsValidator,
+  createPostValidator,
+  deletePostValidator,
+} = require("../middlewares/validators/posts.validator");
 
-router.get("/posts", verifyAuth, getPosts);
-router.post("/posts", verifyAuth, createPost);
-router.delete("/posts/:postId", verifyAuth, deletePost);
+router.get("/posts", getPostsValidator, verifyAuth, getPosts);
+router.post("/posts", createPostValidator, verifyAuth, createPost);
+router.delete("/posts/:postId", deletePostValidator, verifyAuth, deletePost);
 
 module.exports = router;
