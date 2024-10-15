a0473d7cd7ddcbbf9673e91b46eb2c1b27e334c8
Ali Talha Çoban <cobanalitalha@gmail.com>
2024-10-15 11:23:53 +0300

    Refactor stories route to include validation middleware

--- src/routes/stories.route.js
+++ src/routes/stories.route.js
@@ -6,9 +6,19 @@ const {
 } = require("../controllers/stories.controller");
 
 const verifyAuth = require("../middlewares/verifyAuth");
+const {
+  getStoriesValidator,
+  addStoryValidator,
+  deleteStoryValidator,
+} = require("../middlewares/validators/stories.validator");
 
-router.get("/stories", verifyAuth, getStories);
-router.post("/stories", verifyAuth, addStory);
-router.delete("/stories/:storyId", verifyAuth, deleteStory);
+router.get("/stories", getStoriesValidator, verifyAuth, getStories);
+router.post("/stories", addStoryValidator, verifyAuth, addStory);
+router.delete(
+  "/stories/:storyId",
+  deleteStoryValidator,
+  verifyAuth,
+  deleteStory
+);
 
 module.exports = router;
